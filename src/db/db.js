589e77406e58e7222e2bd8c67f8c966f90b842b7
VivekDudi-Github <vivekdudi18@gmail.com>
2024-09-06 12:59:38 +0530

    db connection build

---
+++ src/db/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+import { DB_NAME } from "../contants.js";
+
+
+const connectDB = async() => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`) ;
+        console.log(`MONGODB connected!! || DB HOST : ${connectionInstance.connection.host} `)
+    
+    } catch (error) {
+        console.log('MONGODB connection error @db.js line:12')
+        process.exit(0)
+    }
+}
+
+export {connectDB}
