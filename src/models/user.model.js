64c497edf681ba3828f158b52f7a2e0683392d9b
VivekDudi-Github <vivekdudi18@gmail.com>
2024-09-07 14:42:09 +0530

    usermodel updated

---
+++ src/models/user.model.js
@@ -0,0 +1,77 @@
+import {mongoose , Schema} from "mongoose";
+import jwt from 'jsonwebtoken'
+import bcrypt from "bcrypt"
+
+
+const user_Schema  = new Schema({
+    username : {
+        type : String , 
+        required : true  , 
+        unique : true , 
+        lowerCase : true ,
+        index : true  , //learn about the indexes and trim 
+    } , 
+    email : {
+        type : String , 
+        unique : true , 
+        index : true  , 
+        lowerCase : true , 
+        required : true , 
+    } , 
+    fullname : {
+        type : Text , 
+        required : true , 
+        index : true , 
+    } ,
+    password : {
+        type : String , 
+        unique : true , 
+        required : [true , 'Password is required'] ,
+
+    } , 
+    avatar : {
+        type : String , 
+        unique : true , 
+        required : true ,
+    } , 
+    coverImage : {
+        type : String , 
+        unique : true , 
+        required : true ,
+    } , 
+    watchHistory : [{
+        type : Schema.Types.ObjectId , 
+        ref : 'videos'
+    }] , 
+    refreshToken : {
+        type : String , 
+    } , 
+
+} , {timestamps : true })
+
+
+user_Schema.pre('save' , async function (next) {
+    if( !this.isModified('password'))
+        return next  
+
+    this.password = bcrypt.hash(this.password , 8)
+    next()
+})
+
+user_Schema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password , this.password)
+}
+
+user_Schema.methods.generateAccessToken = function(){
+    jwt.sign({
+        _id : this.id , 
+        email : this.email , 
+        username : this.username  ,
+        fullname : this.fullname 
+    })
+}
+
+user_Schema.methods.generateRefreshToken = function(){}
+
+
+export const user = mongoose.model('user' , user_Schema)
---
+++ src/models/video.model.js
@@ -0,0 +1,44 @@
+import {mongoose , Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+
+const video_Schema  = new Schema({
+    videofile : {
+        type : true , 
+        required : [true , 'Please upload a valid file'] ,
+    } , 
+    thumbnail : {
+        type : String  , 
+        required : true , 
+    } , 
+    title : {
+        type : String , 
+        required  : true 
+    } , 
+    description : {
+        type : String , 
+    } , 
+    duration : {
+        type : Number , 
+        required : true 
+    } , 
+    views : {
+        type : Number , 
+        default : 0 ,
+    } , 
+    isPublished : {
+        type : boolean ,
+        required : true , 
+        default : ture ,
+    } , 
+    owner : {
+        type : Schema.Types.ObjectId , 
+        ref : 'user'
+    }
+    } , {timestamps : true }
+)
+
+video_Schema.plugin(mongooseAggregatePaginate)
+
+
+export const videos = mongoose.model('videos' , video_Schema)
