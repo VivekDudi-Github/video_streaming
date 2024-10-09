20c241413b8ed8b38002be4367caf5b311500188
VivekDudi-Github <vivekdudi18@gmail.com>
2024-10-09 20:00:51 +0530

    added a dashboard controller

--- src/app.js
+++ src/app.js
@@ -24,6 +24,7 @@ import healthCheckRouter from './routes/healthcheck.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js'
 import videoRouter from './routes/video.routes.js'
 import commentRouter from "./routes/comment.routes.js"
+import dashBoardRouter from "./routes/dashboard.routes.js"
 
 //
 app.use("/api/v1/users" , userRouter)
@@ -31,6 +32,8 @@ app.use("/api/v1/healthcheck" , healthCheckRouter)
 app.use("/api/v1/subscriptions" , subscriptionRouter)  
 app.use("/api/v1/videos" , videoRouter)
 app.use('/api/v1/comment' ,  commentRouter)
+app.use('/api/v1/dashboard' ,  dashBoardRouter)
+
 
 
 export {app}  
--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose"
-import {Video} from "../models/video.model.js"
-import {Subscription} from "../models/subscription.model.js"
-import {Like} from "../models/like.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
+import {Videos} from "../models/videos.model.js"
+import {Subscription} from "../models/subscriptions.model.js"
+// import {Like} from "../models/like.model.js"
+import {ApiError} from "../utils/apiErrors.js"
+import {ApiResponse} from "../utils/apiRes.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { Comment } from "../models/comment.model.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
@@ -12,6 +13,32 @@ const getChannelStats = asyncHandler(async (req, res) => {
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+    const {userId} = req.user?._id
+    const {page , pagesize , sort} = req.body
+    
+    if (!userId ) {
+        throw new ApiError(400 , "userid is missing")
+    }
+
+    if (!skip || !limit || !sort) {
+        throw new ApiError(400 , "required json.params are missing")
+    }
+    const limit = pagesize ;
+    const skip = page-1 * pagesize ;
+
+    const videosDocs = await Videos.find({owner : userId})
+                                        .skip(skip)
+                                        .limit(limit)
+                                        .sort(sort)
+    
+    if (!videosDocs) {
+        throw new ApiError(500 , "now videos were found");
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200 , videosDocs , "Videos fetched successfully")
+    )
+
 })
 
 export {
