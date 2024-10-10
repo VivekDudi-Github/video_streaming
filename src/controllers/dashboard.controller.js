93963c4522e2136b76c6025bdf477514e71e7702
VivekDudi-Github <vivekdudi18@gmail.com>
2024-10-10 20:55:32 +0530

    minor bug fixes

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -13,26 +13,27 @@ const getChannelStats = asyncHandler(async (req, res) => {
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
-    const {userId} = req.user?._id
+    const {userId} = req.params
     const {page , pagesize , sort} = req.body
     
     if (!userId ) {
         throw new ApiError(400 , "userid is missing")
     }
 
-    if (!skip || !limit || !sort) {
+    if (!page || !pagesize || !sort) {
         throw new ApiError(400 , "required json.params are missing")
     }
     const limit = pagesize ;
     const skip = page-1 * pagesize ;
 
+    //sort : Sets the sort order. If an object is passed, values allowed are asc, desc, ascending, descending, 1, and -1.
     const videosDocs = await Videos.find({owner : userId})
                                         .skip(skip)
                                         .limit(limit)
                                         .sort(sort)
     
-    if (!videosDocs) {
-        throw new ApiError(500 , "now videos were found");
+    if (videosDocs.length < 1) {
+        throw new ApiError(500 , "no videos were found");
     }
 
     return res.status(200).json(
